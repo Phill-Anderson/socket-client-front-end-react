e73603c7cf27aab54cdbbe97c4539985c6affab0
Phill Anderson <eaglesquad86@gmail.com>
2021-10-15 13:13:27 +0800

    энгийн чат код

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react'
 import { TextField } from '@material-ui/core'
 import useSocketService from './socket/useSocketService'
-
+import { io } from "socket.io-client";
+const socket = io('http://localhost:5000')
 function App() {
-  const [connectionStatus, socket] = useSocketService()
+  const [connectionStatus] = useSocketService()
 
   const [state, setState] = useState({ message: '', name: '' })
-  const [chat, setChat] = useState([])
+  const [chats, setChats] = useState([])
+  /*   useEffect(() => {
+      socket.on('message', ({ name, message }) => {
+        setChat([...chat, { name, message }])
+        console.log(`${name} -- ${message}`)
+      })
+    }) */
   useEffect(() => {
-    socket.on('message', ({ name, message }) => {
-      setChat([...chat, { name, message }])
-    })
+    console.log(`chats:`, chats)
+  }, [chats])
 
-  })
   const onTextChange = e => {
     setState({ ...state, [e.target.name]: e.target.value })
   }
@@ -21,19 +26,17 @@ function App() {
   const onMessageSubmit = (e) => {
     e.preventDefault()
     const { name, message } = state
-    socket.emit('message', { name, message })
+    socket.emit('message', { name, message }, (response) => {
+      const { name, message } = response
+      setChats([...chats, { name, message }])
+    })
     setState({ message: '', name })
   }
-  const renderChat = () => {
-    return chat.map(({ name, message }, index) => {
-      <div key={index}>
-        <h3>{name}: <span>{message}</span></h3>
-      </div>
-    })
-  }
+
   return (
     <>
       <h5 style={{ textAlign: 'center' }}>{connectionStatus}</h5>
+      <pre> {JSON.stringify(chats)}</pre>
       <div className="card">
         <form onSubmit={onMessageSubmit}>
           <h1>Мессэж</h1>
@@ -47,7 +50,16 @@ function App() {
         </form>
         <div className='render-chat'>
           <h1>Chat Log</h1>
-          {renderChat()}
+          {
+            chats.length > 0 ? chats.map((el, index) => {
+              const { name, message } = el
+              return (
+                <div key={index}>{name} : {message}</div>
+              )
+            })
+              :
+              'энэ chats массивт юу ч алга'
+          }
         </div>
       </div>
     </>
--- src/socket/useSocketService.js
+++ src/socket/useSocketService.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { io } from "socket.io-client";
 const socket = io('http://localhost:5000')
 const useSocketService = () => {
-    const [connectionStatus, setConnectionStatus] = useState(null)
+    const [connectionStatus, setConnectionStatus] = useState('холбогдоогүй')
 
     useEffect(() => {
         socket.on('connect', () => {
@@ -18,6 +18,25 @@ const useSocketService = () => {
             console.log(arg)
         })
 
+        socket.emit('callback turshilt', '1', { name: 'updated' }, (response) => {
+            console.log(response.status)
+        })
+
+        socket.emit('hey', 'front талаас мэдээлэл дамжууллаа')
+
+        /*  let count = 0
+         setInterval(() => {
+             socket.volatile.emit('ping', ++count)
+         }, 1000); */
+        socket.emit('one time', 'нэг удаа ажиллах сокет')
+
+        //backend талд нь сокетод validation тавьсан
+        socket.emit('user:create', { username: 'phill', email: 'test@gmail.com' }, (response) => {
+            console.log(response)
+        })
+        /*    socket.on('listenAllClients', (arg) => {
+               console.log(arg)
+           }) */
         return () => {
             socket.disconnect()
         }
